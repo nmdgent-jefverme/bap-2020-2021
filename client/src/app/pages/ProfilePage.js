b477f553ecd51b1c656ccccce5898fe2abe1be97
nmdgent-jefverme <jefvermeireown@gmail.com>
2021-03-24 15:28:10 +0100

    updated profilepage styling

--- client/src/app/pages/ProfilePage.js
+++ client/src/app/pages/ProfilePage.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import profileIcon from '../assets/profileIcon.png';
+
 import {
   Card,
   Navigation,
@@ -12,8 +14,11 @@ const ProfilePage = () => {
       <Navigation activePage='profile'/>
       <PageTitle title='Mijn account'/>
       <div className='container'>
-        <Card>
-          <h2>Mijn profiel</h2>
+        <Card extraClass='profilepage--overviewcard'>
+          <div className='profilepage--image-container'>
+            <img src={profileIcon} alt='Profile icon' className='profilepage--image' />
+          </div>
+          <h3>Mijn profiel</h3>
         </Card>
       </div>
     </>
